bc4a82387786556f22d7276c5c3f398a0701a888
Jan Evseev <jan.evseev@gmail.com>
2022-05-31 11:26:17 +0200

    Refactoring

--- src/command.ts
+++ src/command.ts
@@ -1,42 +1,41 @@
-import { Command, Option } from 'commander';
+import { Command, Option } from "commander";
 
 type LoadBotParam = {
   rpc: URL;
-
-  mode: 'plain' | 'contract';
-
+  mode: "plain" | "contract";
   amount: number;
   rate: number;
   sender: string;
   chainID: number;
 };
-export const program = new Command('avalanche-loadbot');
+
+export const program = new Command("avalanche-loadbot");
 export function initProgram() {
   program
-    .name('avalanche-loadbot')
-    .version(`avalanche-loadbot ${require('../package.json').version}`);
+    .name("avalanche-loadbot")
+    .version(`avalanche-loadbot ${require("../package.json").version}`);
 
   program
     .option(
-      '--rpc <URL>',
+      "--rpc <URL>",
       "URL of Avalanche node's RPC listener, should be full path",
       (val) => {
         return new URL(val);
       },
-      new URL('http://127.0.0.1:9650/ext/bc/C/rpc')
+      new URL("http://127.0.0.1:9650/ext/bc/C/rpc")
     )
 
     .option(
-      '--amount <number>',
-      'Amount N of total transactions to submit',
+      "--amount <number>",
+      "Amount N of total transactions to submit",
       (val) => {
         return parseInt(val);
       },
       1000
     )
     .option(
-      '--chain-id <number>',
-      'Chain ID of Avalanche',
+      "--chain-id <number>",
+      "Chain ID of Avalanche",
       (val) => {
         return parseInt(val);
       },
@@ -44,28 +43,28 @@ export function initProgram() {
     )
     // NOTE Unused currently
     .option(
-      '--rate <number>',
-      'Target submission rate',
+      "--rate <number>",
+      "Target submission rate",
       (val, prev) => {
         return parseInt(val);
       },
       100
     )
     .option(
-      '--sender <address>',
-      'Sender Address',
+      "--sender <address>",
+      "Sender Address",
       (val) => {
-        return '';
+        return "";
       },
-      ''
+      ""
     );
   program.addOption(
-    new Option('--mode <plain, contract>', 'Mode to operation')
-      .choices(['plain', 'contract'])
-      .default('contract')
+    new Option("--mode <plain, contract>", "Mode to operation")
+      .choices(["plain", "contract"])
+      .default("contract")
   );
 
-  program.on('command:*', () => {
+  program.on("command:*", () => {
     program.help();
     process.exitCode = 1;
   });
